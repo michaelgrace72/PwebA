e22a602c8fb8eb5ca9a981c50e658374f69f9b85
Mikha Gracia Sugiono <114130032+michaelgrace72@users.noreply.github.com>
2023-11-20 18:25:47 -0600

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "@Saya.Mikha72";
+$database = "mynotescode";
+// Koneksi ke MySQL dengan PDO
+$pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
+?>
